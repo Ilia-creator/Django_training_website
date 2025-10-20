6414e09ff2bcbb17d64dd5fe54c45e92f3d4b1dc
Ilia Shubnikov <ilja.shubnikov@gmail.com>
2025-10-20 20:42:18 +0300

    Add static assets and DB migrations

---
+++ price/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class PriceCard(models.Model):
+    pc_value = models.CharField(max_length=20, verbose_name='Price')
+    pc_description = models.CharField(max_length=200, verbose_name='Description')
+
+    def __str__(self):
+        return self.pc_value
+
+
+
+class PriceTable(models.Model):
+    pt_title = models.CharField(max_length=200, verbose_name='Service')
+    pt_old_price = models.CharField(max_length=200, verbose_name='Old price')
+    pt_new_price = models.CharField(max_length=200, verbose_name='New price')
+
+    def __str__(self):
+        return self.pt_title
